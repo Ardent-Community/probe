4618b725c1ccdae904fc1d6efe5735e51b2f2063
Shravan Asati <dev.shravan@protonmail.com>
2021-06-30 20:52:49 +0530

    💄 added the cli

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Ardent-Community/probe
 
 go 1.16
+
+require github.com/thatisuday/commando v1.0.4
--- probe.go
+++ probe.go
@@ -11,44 +11,67 @@ package main
 
 import (
 	"fmt"
+	"github.com/thatisuday/commando"
 	serve "github.com/Ardent-Community/probe/services"
 	"sync"
 )
 
+const (
+	NAME string = "probe"
+	VERSION string = "0.1.0"
+)
+
 func main() {
-	wg := sync.WaitGroup{}
-	solutions := serve.GetSolutions("1").Solutions
+	fmt.Println(NAME, VERSION)
 
-	winners := []string{}
-	wg.Add(len(solutions))
+	commando.
+		SetExecutableName(NAME).
+		SetVersion(VERSION).
+		SetDescription("probe is a CLI tool made to automate the process of solution validation for weekly challenges conducted in the Ardent-Community.\n")
 
-	for username, data := range solutions {
-		go func(username string, data map[string]string) {
-			lang := data["language"]
-			code := data["code"]
-			serve.Log("info", "running "+username+"'s solution written in "+lang)
+	commando.
+		Register("run").
+		AddArgument("challengeNumber", "The challenge number to validate the solutions for.", "").
+		AddArgument("testCasesFile", "The path to the test cases file.", "").
+		SetAction(func(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
 
-			t := serve.Tester{
-				Lang:          lang,
-				Code:          code,
-				TestCasesFile: "./examples/testcases.json",
+			wg := sync.WaitGroup{}
+			solutions := serve.GetSolutions(args["challengeNumber"].Value).Solutions
+		
+			winners := []string{}
+			wg.Add(len(solutions))
+		
+			for username, data := range solutions {
+				go func(username string, data map[string]string) {
+					lang := data["language"]
+					code := data["code"]
+					serve.Log("info", "running "+username+"'s solution written in "+lang)
+		
+					t := serve.Tester{
+						Lang:          lang,
+						Code:          code,
+						TestCasesFile: args["testCasesFile"].Value,
+					}
+					passed := t.PerformTests()
+					if passed {
+						serve.Log("success", username+"'s code passed")
+						winners = append(winners, username)
+					} else {
+						serve.Log("failure", username+"'s code failed")
+					}
+					wg.Done()
+				}(username, data)
 			}
-			passed := t.PerformTests()
-			if passed {
-				serve.Log("success", username+"'s code passed")
-				winners = append(winners, username)
-			} else {
-				serve.Log("failure", username+"'s code failed")
+		
+			wg.Wait()
+			serve.ClearClutter()
+		
+			serve.Log("info", "\nThe winners are:\n")
+			for i, v := range winners {
+				serve.Log("success", fmt.Sprintf("%v. %v", i+1, v))
 			}
-			wg.Done()
-		}(username, data)
-	}
+		} )
 
-	wg.Wait()
-	serve.ClearClutter()
+	commando.Parse(nil)
 
-	serve.Log("info", "\nThe winners are:\n")
-	for i, v := range winners {
-		serve.Log("success", fmt.Sprintf("%v. %v", i+1, v))
-	}
 }
