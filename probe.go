580b700fd5b7d4cd31ad4fa606276b6f5a3f9c3d
Shravan Asati <dev.shravan@protonmail.com>
2021-07-01 12:21:59 +0530

    📝 added `run` command description and the root command

--- probe.go
+++ probe.go
@@ -11,13 +11,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/thatisuday/commando"
 	serve "github.com/Ardent-Community/probe/services"
+	"github.com/thatisuday/commando"
 	"sync"
 )
 
 const (
-	NAME string = "probe"
+	NAME    string = "probe"
 	VERSION string = "0.1.0"
 )
 
@@ -30,23 +30,31 @@ func main() {
 		SetDescription("probe is a CLI tool made to automate the process of solution validation for weekly challenges conducted in the Ardent-Community.\n")
 
 	commando.
+		Register(nil).
+		SetAction(func(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
+			fmt.Println("\nExecute `probe -h` for help.")
+		})
+
+	commando.
 		Register("run").
+		SetShortDescription("Validates solutions.").
+		SetDescription("The `run` command does, in order, makes a request to the API, grabs the solutions, and concurrently runs all the solutions against the test cases.").
 		AddArgument("challengeNumber", "The challenge number to validate the solutions for.", "").
 		AddArgument("testCasesFile", "The path to the test cases file.", "").
 		SetAction(func(args map[string]commando.ArgValue, flags map[string]commando.FlagValue) {
 
 			wg := sync.WaitGroup{}
 			solutions := serve.GetSolutions(args["challengeNumber"].Value).Solutions
-		
+
 			winners := []string{}
 			wg.Add(len(solutions))
-		
+
 			for username, data := range solutions {
 				go func(username string, data map[string]string) {
 					lang := data["language"]
 					code := data["code"]
 					serve.Log("info", "running "+username+"'s solution written in "+lang)
-		
+
 					t := serve.Tester{
 						Lang:          lang,
 						Code:          code,
@@ -62,15 +70,15 @@ func main() {
 					wg.Done()
 				}(username, data)
 			}
-		
+
 			wg.Wait()
 			serve.ClearClutter()
-		
+
 			serve.Log("info", "\nThe winners are:\n")
 			for i, v := range winners {
 				serve.Log("success", fmt.Sprintf("%v. %v", i+1, v))
 			}
-		} )
+		})
 
 	commando.Parse(nil)
 
